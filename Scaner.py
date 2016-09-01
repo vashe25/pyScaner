18b5bb8df5f8c9fd315eb54707fbfcc538d680b6
CETUS <v.she25@gmail.com>
2016-09-02 00:35:03 +0300

    sugar

--- Scaner.py
+++ Scaner.py
@@ -2,14 +2,15 @@ import os, sys
 
 class scaner:
 	"""docstring for scaner"""
-	def __init__(self, os_object):
-		self.os = os_object
 	# current dir
 	current_dir = os.getcwd()
 	# pattern to find
 	pattern = ".py"
 	# list of founded files
 	files = []
+	
+	def __init__(self, os_object):
+		self.os = os_object
 
 	def scanFor(self):
 		for items in self.os.listdir(self.current_dir):
